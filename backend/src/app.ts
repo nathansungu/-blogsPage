4f889e6aa183fb29deafedc0092fc31e435e7d0b
nathansungu <nathanamudavi@gmail.com>
2025-07-09 15:58:42 +0300

    add routes importation in app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express, {Express, json} from "express";
+import auth from "./routes/authentication/auth.route"
+import blogs from "./routes/blogs.routes.ts/blogs";
+import user from "./routes/user.routes/user.routes";
+import errorHandler from "./middlewares/errorHandler.middleware";
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", auth)
+app.use("/api/blogs", blogs)
+app.use("/api/user", user)
+app.use(errorHandler)
+
+const port = process.env.PORT || 4000;
+app.listen(port, ()=> console.log(`app is listening on port  ${port}`))
+
