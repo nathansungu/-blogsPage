7535cbabfa3fd34dcbc0b4f1a7700bdc4f73277a
nathansungu <nathanamudavi@gmail.com>
2025-07-11 14:51:53 +0300

    add error handler on server if port is in use

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,9 +4,16 @@ import blogs from "./routes/blogs.routes.ts/blogs";
 import user from "./routes/user.routes/user.routes";
 import errorHandler from "./middlewares/errorHandler.middleware";
 import cookieParser from 'cookie-parser';
+//add cors
+import cors from 'cors';
+
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true, 
+}));
 
 app.use("/api/auth", auth)
 app.use("/api/blogs", blogs)
@@ -14,5 +21,9 @@ app.use("/api/user", user)
 app.use(errorHandler)
 
 const port = process.env.PORT || 4000;
-app.listen(port, ()=> console.log(`app is listening on port  ${port}`))
+app.listen(port, (e)=> {
+    e?console.error(e):
+    console.log(`app is listening on port  ${port}`)
+
+})
 
