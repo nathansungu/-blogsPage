feba12794d57038261697a4f7fb7d6586aaa5b20
nathansungu <nathanamudavi@gmail.com>
2025-07-12 18:57:30 +0300

    add get user routes

--- backend/src/controller/blogs/getBlogs.controller.ts
+++ backend/src/controller/blogs/getBlogs.controller.ts
@@ -5,21 +5,20 @@ import asyncHandler from "../../utilities/asyncHandler";
 const client = new PrismaClient();
 
 const getBlogs = asyncHandler(async (req: Request, res: Response) => {
-  
   const blogs = await client.posts.findMany({
     where: { isDeleted: false },
-    include:{
-      user:{
-        select:{
-          firstName:true,
-          secondName:true
-        }
-      }
-    }
+    include: {
+      user: {
+        select: {
+          firstName: true,
+          secondName: true,
+        },
+      },
+    },
   });
 
   if (blogs) {
-        res.status(200).send({ blogs: blogs });
+    res.status(200).send({ blogs: blogs });
     return;
   } else {
     res.status(404).send({ message: "No blogs found." });
