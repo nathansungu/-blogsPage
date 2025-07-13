2a49e2ec8aba13124fc4bd0e27f68835426bc102
nathansungu <nathanamudavi@gmail.com>
2025-07-13 15:03:40 +0300

    add profile page

---
+++ frontend/src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import DashboardHeader from "../components/dasboard.components/header";
+import HandleUserProfile from "../components/userProfile.component/userProfile"
+
+const Profile = ()=>{
+    return(
+        <>
+        <DashboardHeader/>
+        <HandleUserProfile/>
+        </>
+    )
+}
+
+export default Profile;
