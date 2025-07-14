25d013bd902318588d83aee1fd42697214a64556
nathansungu <nathanamudavi@gmail.com>
2025-07-14 07:23:54 +0300

    add vercel route on couse origin

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://blogs-page-two.vercel.app/',
     credentials: true, 
 }));
 
